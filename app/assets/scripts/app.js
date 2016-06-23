86b9aef97758ff75e6ad3d1d5340eaf144f9b2c6
Jacques tatiana <tatout08090@gmail.com>
2016-06-23 20:35:35 +0200

    [FRONT - HTML -JS - SASS] Add parameters

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -33,13 +33,57 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //notifications
 
+    var body = document.querySelector('.container');
+
     var not = document.querySelector('header .not');
     var notIcon = document.querySelector('header .not .icon');
-    var cNotifications = document.querySelector('.c-sub-header');
+
+    var param = document.querySelector('header .informations');
+    var paramIcon = document.querySelector('header .informations .icon');
+
+
+    var cNotifications = document.querySelector('.c-sub-header.c-notifications');
+    var cParam = document.querySelector('.c-sub-header.c-param');
 
     not.addEventListener('click', function() {
+        cParam.classList.remove('isShow');
         cNotifications.classList.toggle('isShow');
         notIcon.classList.toggle('isActive');
+        paramIcon.classList.remove('isActive');
+        body.classList.remove('overlay');
+
+        if(cNotifications.classList.contains('isShow')){
+            body.classList.add('overlay');
+        }else{
+            body.classList.remove('overlay');
+        }
+
+    });
+
+    //param
+
+    param.addEventListener('click', function() {
+        cNotifications.classList.remove('isShow');
+        cParam.classList.toggle('isShow');
+        paramIcon.classList.toggle('isActive');
+        notIcon.classList.remove('isActive');
+        body.classList.remove('overlay');
+
+        if(cParam.classList.contains('isShow')){
+            body.classList.add('overlay');
+        }else{
+            body.classList.remove('overlay');
+        }
+
+    });
+
+    body.addEventListener('click', function() {
+        cNotifications.classList.remove('isShow');
+        cParam.classList.remove('isShow');
+        paramIcon.classList.remove('isActive');
+        notIcon.classList.remove('isActive');
+        body.classList.remove('overlay');
+
     });
 
 })
